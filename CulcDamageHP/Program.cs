ffee1e940313b5ce477dff01ba2b749b85b2f4b8
lexxA87 <alex.o.nikonov@gmail.com>
2022-11-17 18:44:51 +0400

    Added switch-case to Program.Main()

--- CulcDamageHP/Program.cs
+++ CulcDamageHP/Program.cs
@@ -6,52 +6,47 @@
 
         static void Main(string[] args)
         {
-            SwordDamage swordDamage = new(RollDice());
-            ArrowDamage arrowDamage = new(RollDice());
+            SwordDamage swordDamage = new(RollDice(3));
+            ArrowDamage arrowDamage = new(RollDice(1));
 
             while (true)
             {
                 Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
                 char key = Console.ReadKey().KeyChar;
 
-                swordDamage = new(RollDice());
-
                 if (key != '0' && key != '1' && key != '2' && key != '3') return;
 
-                if (key == '0')
-                {
-                    swordDamage.Magic = false;
-                    swordDamage.Flaming = false;
-                }
-                else if (key == '1')
-                {
-                    swordDamage.Magic = true;
-                }
-                else if (key == '2')
-                {
-                    swordDamage.Flaming = true;
-                }
-                else
-                {
-                    swordDamage.Magic = true;
-                    swordDamage.Flaming = true;
-                }
+                Console.Write("\nS for sword, A for arrow, anything else to quit: ");
+                char weaponKey = Char.ToUpper(Console.ReadKey().KeyChar);
 
-                Console.WriteLine("\nRolled " + swordDamage.Roll + " for " + swordDamage.Damage + " HP\n");
+                switch (weaponKey)
+                {
+                    case 'S':
+                        swordDamage.Roll = RollDice(3);
+                        swordDamage.Magic = (key == '1' || key == '3');
+                        swordDamage.Flaming = (key == '2' || key == '3');
+                        Console.WriteLine($"\nRolled {swordDamage.Roll} for {swordDamage.Damage} HP\n");
+                        break;
+                    case 'A':
+                        arrowDamage.Roll = RollDice(1);
+                        arrowDamage.Magic = (key == '1' || key == '3');
+                        arrowDamage.Flaming = (key == '2' || key == '3');
+                        Console.WriteLine($"\nRolled {arrowDamage.Roll} for {arrowDamage.Damage} HP\n");
+                        break;
+                    default:
+                        return;
+                }
             }
         }
 
         static int RollDice(int numberOfRolls)
         {
-            switch (numberOfRolls)
+            int roll = 0;
+            for (int i = 0; i < numberOfRolls; i++)
             {
-                case 1:
-                    return random.Next(1, 7);
-                case 3:
-                    return random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
-                default:
-                    return 0;
+                roll += random.Next(1, 7);
             }
+            return roll;
         }
     }
 }
