9f40f649e325454010f2e6669a04defa9f28dca5
lexxA87 <alex.o.nikonov@gmail.com>
2022-11-18 19:51:47 +0400

    Added superclass WeaponDamage for arrow and sword classes

--- CulcDamageHP/ArrowDamage.cs
+++ CulcDamageHP/ArrowDamage.cs
@@ -1,66 +1,15 @@
 ﻿namespace CulcDamageHP
 {
-    internal class ArrowDamage
+    internal class ArrowDamage : WeaponDamage
     {
-        public ArrowDamage(int roll)
-        {
-            Roll = roll;
-            Magic = false;
-            Flaming = false;
-            CalculateDamage();
-        }
+        public ArrowDamage(int roll) : base(roll) { }
+
 
         private const decimal BASE_MULTYPLIER = 0.35M;
         private const decimal MAGIC_MULTYPLIER = 2.5M;
         private const decimal FLAME_DAMAGE = 1.25M;
 
-        /// <summary>
-        /// int Damage is calculated sum all damages
-        /// </summary>
-        public int Damage { get; private set; }
-
-        private int roll;
-        /// <summary>
-        /// int Roll is sum three dice
-        /// </summary>
-        public int Roll
-        {
-            get { return roll; }
-            set
-            {
-                roll = value;
-                CalculateDamage();
-            }
-        }
-
-        private bool flaming;
-        /// <summary>
-        /// bool Flaming for calculate flaming damage
-        /// </summary>
-        public bool Flaming
-        {
-            get { return flaming; }
-            set
-            {
-                flaming = value;
-                CalculateDamage();
-            }
-        }
-
-        private bool magic;
-        /// <summary>
-        /// bool Magic for calculate magic damage
-        /// </summary>
-        public bool Magic
-        {
-            get { return magic; }
-            set
-            {
-                magic = value;
-                CalculateDamage();
-            }
-        }
-        private void CalculateDamage()
+        protected override void CalculateDamage()
         {
             decimal baseDamage = Roll * BASE_MULTYPLIER;
             if (Magic)
---
+++ CulcDamageHP/WeaponDamage.cs
@@ -0,0 +1,62 @@
+﻿namespace CulcDamageHP
+{
+    internal class WeaponDamage
+    {
+        public WeaponDamage(int roll)
+        {
+            Roll = roll;
+            Magic = false;
+            Flaming = false;
+            CalculateDamage();
+        }
+
+        public int Damage { get; protected set; }
+
+        private int roll;
+        /// <summary>
+        /// int Roll is sum three dice
+        /// </summary>
+        public int Roll
+        {
+            get { return roll; }
+            set
+            {
+                roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+        /// <summary>
+        /// bool Flaming for calculate flaming damage
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool magic;
+        /// <summary>
+        /// bool Magic for calculate magic damage
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        protected virtual void CalculateDamage()
+        {
+            // overrides in subclasses
+        }
+    }
+}
